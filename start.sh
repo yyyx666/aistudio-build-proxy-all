f5ac7843ef0b69b1bb93faa14a0b0209be4b76df
yyyx666 <2652877047@qq.com>
2025-09-10 01:02:54 +0800

    Add files via upload

--- start.sh
+++ start.sh
@@ -9,4 +9,17 @@ if [ ! -z "$COOKIE" ]; then
 	echo "载入user1_cookie"
 fi
 
+if [ -n "$CONFIG_URL" ]; then
+    # 使用curl下载
+    curl -fsSL "$CONFIG_URL" -o /app/config.yaml
+	echo "载入config.yaml"
+else
+
+if [ -n "$COOKIE_URL" ]; then
+    # 使用curl下载
+    curl -fsSL "$CONFIG_URL" -o /app/cookies/user1_cookie.json
+	echo "载入user1_cookie"
+else
+
+
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
