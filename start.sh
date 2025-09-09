224febb590e1bdf65e2108e7c4d742ed3a51201c
yyyx666 <2652877047@qq.com>
2025-09-10 01:43:04 +0800

    Add files via upload

--- start.sh
+++ start.sh
@@ -11,8 +11,15 @@ fi
 
 if [ -n "$CONFIG_URL" ]; then
     # 使用curl下载
-    curl -fsSL "$CONFIG_URL" -o /opt/haoren/data/config.json
-else
+    curl -fsSL "$CONFIG_URL" -o /app/config.yaml
+	echo "载入config.yaml"
+fi
+
+if [ -n "$COOKIE_URL" ]; then
+    # 使用curl下载
+    curl -fsSL "$COOKIE_URL" -o /app/cookies/user1_cookie.json
+	echo "载入user1_cookie"
+fi
 
 
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
